7806e8b6adcb465063e396092edc8ec2c109fbe7
153322219-myseneca <132936663+153322219-myseneca@users.noreply.github.com>
2023-06-13 17:52:29 -0400

    create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,7 @@
+def calculate_age():
+    current_year = 2021  # set the current year
+    birth_year = int(input("Enter your birth year: "))  # ask the user for their birth year and convert it to an integer
+    age = current_year - birth_year  # calculate the age
+    print("Your age is", age)  # print the age
+
+calculate_age()  # call the function
