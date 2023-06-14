1e709b14f7343d347f2c59e0877a3d83d47050cc
devank <devank@myseneca.ca>
2023-06-13 21:00:26 -0400

    Text added to Lab 1

--- Lab1.py
+++ Lab1.py
@@ -1,5 +1,5 @@
 def calculate_age():
-    current_year = 2021  # set the current year
+    current_year = 2023  # set the current year
     birth_year = int(input("Enter your birth year: "))  # ask the user for their birth year and convert it to an integer
     age = current_year - birth_year  # calculate the age
     print("Your age is", age)  # print the age
@@ -8,7 +8,9 @@ calculate_age()  # call the function
 
 
 def helloWorld():
-	print(‘Hello World’)
+	print('Hello World')
 
 
 helloWorld()
+
+
