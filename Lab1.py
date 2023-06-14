12f1af0847e305af5d2b8e1ef24f03676ded2e9b
devank <devank@myseneca.ca>
2023-06-13 21:21:10 -0400

    Changes necessary

--- Lab1.py
+++ Lab1.py
@@ -1,8 +1,13 @@
 def calculate_age():
     current_year = 2023  # set the current year
-    birth_year = int(input("Enter your birth year: "))  # ask the user for their birth year and convert it to an integer
-    age = current_year - birth_year  # calculate the age
-    print("Your age is", age)  # print the age
+    
+    try:
+        birth_year = int(input("Enter your birth year: "))  # ask the user for their birth year and convert it to an integer
+        age = current_year - birth_year  # calculate the age
+        print("Your age is", age)  # print the age
+        
+    except ValueError:
+        print("Please enter an integer for the birth year.")
 
 calculate_age()  # call the function
 
